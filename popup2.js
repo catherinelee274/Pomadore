b8d8bd8eb680739260f4cfdff25c7b3538e5723c
catherinelee274 <catherinelee274@yahoo.com>
2017-04-08 18:17:30 -0700

    Add files via upload

---
+++ popup2.js
@@ -0,0 +1,43 @@
+
+var studyMinutes = 3; //minutes of "focus" 
+var duration = 60 * studyMinutes; //total minutes in seconds
+var display = document.querySelector('#time');
+
+	
+	function timer() {
+		
+		var start = Date.now();
+		var diff, minutes, seconds;
+
+        // get the number of seconds that have elapsed since 
+        // startTimer() was called
+        diff = duration - (((Date.now() - start) / 1000) | 0);
+
+        // does the same job as parseInt truncates the float
+        minutes = (diff / 60) | 0;
+        seconds = (diff % 60) | 0;
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        if (diff <= 0) {
+            // add one second so that the count down starts at the full duration
+            // example 05:00 not 04:59
+            start = Date.now() + 1000;
+        } 
+		
+		console.log("minutes" + minutes);
+		console.log("seconds" + seconds);
+		display.textContent = minutes + ":" + seconds;
+}
+
+
+window.onload = function () {
+	
+	
+	var startButton = document.getElementById('start button');
+	startButton.addEventListener("click",function(){
+	setInterval(timer, 20); //first parameter is thing to be executed, second one is time to wait in milliseconds before it happens again. displays the timer 
+	});
+
+}
